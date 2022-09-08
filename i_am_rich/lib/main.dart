924cd7a2d032b7d3dd58f8aec4df986a4bce4e98
Eduardo Viega <eduardoviega12@gmail.com>
2022-09-08 16:36:36 -0300

    I Am Rich Project

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
