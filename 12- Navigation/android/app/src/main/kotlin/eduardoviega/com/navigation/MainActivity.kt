b860449b32574efac32bda88443c5af8529cba2f
Eduardo Viega <eduardoviega12@gmail.com>
2022-09-26 14:45:54 -0300

    Navigation Challenge

---
+++ 12- Navigation/android/app/src/main/kotlin/eduardoviega/com/navigation/MainActivity.kt
@@ -0,0 +1,6 @@
+package eduardoviega.com.navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 12- Navigation/lib/screen1.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screen2.dart';
+
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Screen 1'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red)),
+          child: Text('Go Forwards To Screen 2'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
